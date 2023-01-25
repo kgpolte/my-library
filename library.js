78b7829aed8689de780e5a396e1fb0a70f103591
Kenny Polte <120602839+kgpolte@users.noreply.github.com>
2023-01-25 15:13:34 -0800

    Change remove button to icon

--- library.js
+++ library.js
@@ -9,13 +9,36 @@ function Book(title, author, pages, read) {
 
 function removeBook(card) {
   myLibrary.splice(card.getAttribute("data-index"), 1);
-  console.log(myLibrary);
   card.remove();
 }
 
 function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
+}
+
+function renderTrashIcon(element) {
+  const iconSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  const iconPath = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path"
+  );
+
+  iconSvg.setAttribute("fill", "gray");
+  iconSvg.setAttribute("viewBox", "0 0 24 24");
+  iconSvg.setAttribute("stroke", "none");
+  iconSvg.setAttribute("height", "2em");
+  iconSvg.setAttribute("width", "2em");
+  iconSvg.classList.add("icon");
+
+  iconPath.setAttribute(
+    "d",
+    "M18,19C18,20.66 16.66,22 15,22H8C6.34,22 5,20.66 5,19V7H4V4H8.5L9.5,3H13.5L14.5,4H19V7H18V19M6,7V19C6,20.1 6.9,21 8,21H15C16.1,21 17,20.1 17,19V7H6M18,6V5H14L13,4H10L9,5H5V6H18M8,9H9V19H8V9M14,9H15V19H14V9Z"
+  );
+
+  iconSvg.appendChild(iconPath);
+
+  return element.appendChild(iconSvg);
 }
 
 function addCard(book) {
@@ -41,12 +64,14 @@ function addCard(book) {
   read.textContent = `${book.read ? "read" : "not read"}`;
   card.appendChild(read);
 
-  const remove = document.createElement("button");
-  remove.setAttribute("type", "button");
-  remove.classList.add("remove-button");
-  remove.textContent = "Remove";
-  remove.addEventListener("click", e => removeBook(e.target.parentElement));
-  card.appendChild(remove);
+  const removeButton = document.createElement("button");
+  removeButton.setAttribute("type", "button");
+  removeButton.classList.add("remove-button");
+  renderTrashIcon(removeButton);
+  removeButton.addEventListener("click", () =>
+    removeBook(removeButton.parentElement)
+  );
+  card.appendChild(removeButton);
 
   bookDisplay.appendChild(card);
 }
@@ -66,11 +91,11 @@ document.getElementById("add").addEventListener("click", () => {
   document.querySelector("input").focus();
 });
 
-document.addEventListener("keydown", e => {
+document.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
     hideForm();
   }
-})
+});
 
 document.getElementById("cancel").addEventListener("click", () => {
   hideForm();
